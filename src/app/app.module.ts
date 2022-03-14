0d1751b06b08a5d0f386478db44d09b54adca7bf
Siddharth5468 <popatsiddharth05@gmail.com>
2022-03-14 14:08:23 +0530

    Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeskboardComponent } from './deskboard/deskboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeskboardComponent
   ],
   imports: [
     BrowserModule,
